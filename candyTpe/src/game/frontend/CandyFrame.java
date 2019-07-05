60bb43a21c9cc46e2c96ad5b534932bb0f2180e2
Nicolás Comerci <ncomerci@itba.edu.ar>
2019-07-05 20:28:45 -0300

    mejoré el manejo del panel extra para los niveles 2 y 3

--- candyTpe/src/game/frontend/CandyFrame.java
+++ candyTpe/src/game/frontend/CandyFrame.java
@@ -89,7 +89,6 @@ public class CandyFrame extends VBox {
 					System.out.println("Get second = " +  newPoint);
 					game().tryMove((int)lastPoint.getX(), (int)lastPoint.getY(), (int)newPoint.getX(), (int)newPoint.getY());
 					String message = ((Long)game().getScore()).toString();
-					String message2 = ((Long)game().getExtraInfo()).toString();
 					if (game().isFinished()) {
 						if (game().playerWon()) {
 							message = message + " Finished - Player Won!";
