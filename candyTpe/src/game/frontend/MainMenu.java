78bb7aa51d93946e48ec6bd3b2c510a77a756a27
Nicolás Comerci <ncomerci@itba.edu.ar>
2019-07-06 13:48:53 -0300

    ...

--- candyTpe/src/game/frontend/MainMenu.java
+++ candyTpe/src/game/frontend/MainMenu.java
@@ -16,7 +16,6 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,7 +45,6 @@ class MainMenu {
 
         Button[] buttons_array = {new Button("Level 1"), new Button("Level 2"), new Button("Level 3"), new Button("\t  Level 2\n(Alternative version)")};
         List<Button> buttons = new ArrayList<>(Arrays.asList(buttons_array));
-
         for(Button button: buttons) {
             button.setFont(new Font("Serif Bold Italic", 40));
             button.setMinSize(200, 100);
@@ -56,14 +54,12 @@ class MainMenu {
 
         Scene mainMenuScene = new Scene(root, 800, 600);
 
+        //Class<?>[] levels = {Level1.class, Level2.class, Level3.class, Level2Aux.class}; //para cuando pueda mejorar lo de abajo
+
         buttons_array[0].setOnMouseClicked(e -> launchLevel(primaryStage, Level1.class));
-
         buttons_array[1].setOnMouseClicked(e -> launchLevel(primaryStage, Level2.class));
-
         buttons_array[2].setOnMouseClicked(e -> launchLevel(primaryStage, Level3.class));
-
         buttons_array[3].setOnMouseClicked(e -> launchLevel(primaryStage, Level2Aux.class));
-
 
         primaryStage.setScene(mainMenuScene);
         primaryStage.show();
